e9467effeb04778b050a9db9e7ba6a6ac9054ac2
Armando Ramirez <armando.m.ramirez@gmail.com>
2025-08-12 17:43:48 -0700

    hash1

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
     defaults = {
       netplay = {
         version = "3.5.0";
-        hash = nixpkgs.lib.fakeHash;
+        hash = "sha256-X8tBAqxURHZ6Jo9fpDB3cu6T3rcls9iYqY6OZW4f/FA=";
       };
       netplay-beta = {
         version = "4.0.0-mainline-beta.8";
